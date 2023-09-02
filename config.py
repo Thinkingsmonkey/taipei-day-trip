76f8d1af77120209d3eb82b7e4e83c9e6ae2eceb
Neal <neal99185@gmail.com>
2023-09-02 21:01:51 +0800

    move config to a file

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config():
   JSON_AS_ASCII = False
   TEMPLATES_AUTO_RELOAD = True
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')  # 使用環境變數
+  SQLALCHEMY_DATABASE_URI = "mysql://root:12345678@localhost/taipeiattractions"  # 開發環境使用
+  # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')  # 部屬環境使用
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   ERROR_404_HELP = False
