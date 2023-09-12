51f2e41e4d1e87a13feecc96511060a13683b9d0
neal <neal9918@gmail.com>
2023-09-13 07:49:43 +0800

    add 9-13 JS_Ninja

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config():
   # JSON_SORT_KEYS = False
   TEMPLATES_AUTO_RELOAD = True
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')  # 部署環境使用
-  # SQLALCHEMY_DATABASE_URI = "mysql://root:12345678@localhost/taipeiattractions"  # 開發環境使用
+  # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')  # 部署環境使用
+  SQLALCHEMY_DATABASE_URI = "mysql://root:12345678@localhost/taipeiattractions"  # 開發環境使用
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   ERROR_404_HELP = False
